7d779a1c6a910ddf83f1803c4db53240ceab9054
Raushan kumar giri <raushan.giri@orangecurrent.com>
2024-10-05 21:33:18 +0100

    cors error fix

--- app.js
+++ app.js
@@ -64,11 +64,12 @@ connectDB(function (err, db) {
 function startexpress() {
   const app = express();
   app.use(express.json({ limit: '2mb' }));
-  app.use(cors({
-    origin: ['https://customer-management-admin.netlify.app','http://localhost:3000','http://82.180.147.224:3000/'], // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // If you need cookies or authorization headers to be sent
-  }));
+  app.use(cors());
+  // app.use(cors({
+  //   origin: ['https://customer-management-admin.netlify.app','http://localhost:3000','http://82.180.147.224:3000/'], // Replace with your frontend URL
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   credentials: true, // If you need cookies or authorization headers to be sent
+  // }));
   app.use(express.urlencoded({ limit: '2mb', extended: true }));
 
   var corsOptions = {
